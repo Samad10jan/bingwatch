7f4425bed284a27fc5bf1854e08710685f1312a5
Samad10jan <abdulsamad10jan@gmail.com>
2025-11-02 18:03:06 +0530

    - added theme change button

--- src/app/(group)/page.tsx
+++ src/app/(group)/page.tsx
@@ -8,11 +8,11 @@ import Image from "next/image";
 export default async function Home() {
  
   let popularAnime, tvAnime, moviesAnime, upcomingAnime, ovaAnime
-  const urlHero = "https://api.jikan.moe/v4/top/anime?limit=10&filter=airing&page=1";
-  const urlTv = "https://api.jikan.moe/v4/top/anime?type=tv&page=1&limit=23";
-  const urlMovies = "https://api.jikan.moe/v4/top/anime?type=movie&page=1&limit=23";
-  const urlUpcoming = "https://api.jikan.moe/v4/top/anime?filter=upcoming&page=1&limit=23";
-  const urlOVA = "https://api.jikan.moe/v4/top/anime?type=ova&page=1&limit=23";
+  const urlHero = "https://api.jikan.moe/v4/top/anime?limit=10&filter=airing&page=1&sfw=";
+  const urlTv = "https://api.jikan.moe/v4/top/anime?type=tv&page=1&limit=23&sfw=1";
+  const urlMovies = "https://api.jikan.moe/v4/top/anime?type=movie&page=1&limit=23&sfw=1";
+  const urlUpcoming = "https://api.jikan.moe/v4/top/anime?filter=upcoming&page=1&limit=23&sfw=1";
+  const urlOVA = "https://api.jikan.moe/v4/top/anime?type=ova&page=1&limit=23&sfw=0";
   try {
 
     const fetchSafe = async (url: string) => {
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -7,12 +7,13 @@ import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
 import { SidebarTrigger } from "@/components/ui/sidebar"
 import { Separator } from "@radix-ui/react-separator"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
 
 
 export function Header() {
-  // const { theme,setTheme } = useTheme()
-  
-  
+  const { theme,setTheme } = useTheme()
+
+
   // function handleTheme(){
   //   if(theme==='dark'){
   //       setTheme('light')
@@ -24,18 +25,40 @@ export function Header() {
   // }
 
   return (
-      <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-          </div>
-          
-        
-          <div className="flex-1 flex justify-center md:justify-start">
-            <h1 className="text-2xl sm:text-3xl md:text-4xl font-extrabold bg-gradient-to-r from-yellow-50 to-yellow-300 bg-clip-text text-transparent">
-              BingWatch
-            </h1>
-          </div>
-        </header>
+    <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+      <div className="flex items-center gap-2 px-4">
+        <SidebarTrigger className="-ml-1" />
+        <Separator orientation="vertical" className="mr-2 h-4" />
+      </div>
+
+
+      <div className="flex-1 flex justify-center md:justify-start">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-extrabold bg-gradient-to-r from-yellow-50 to-yellow-300 bg-clip-text text-transparent">
+          BingWatch
+        </h1>
+      </div>
+      <div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
   )
 }
