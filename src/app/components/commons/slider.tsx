5c8cf82dcac259d7954040b22efa998f5d05629c
Samad10jan <abdulsamad10jan@gmail.com>
2026-03-29 13:10:11 +0530

    -

--- src/app/components/commons/slider.tsx
+++ src/app/components/commons/slider.tsx
@@ -1,30 +1,26 @@
 "use client";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/shadcncomponents/ui/carousel";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/shadcncomponents/ui/carousel";
 import { Anime, CarouselSlideProps, Manga } from "@/lib/type";
 import { useEffect, useState } from "react";
-import AnimeCard from "../anime-components/moviecard";
-import MangaCard from "../manga-components/mangacard";
+import AnimeCard from "../anime-components/moviecard"; // anime card
+import MangaCard from "../manga-components/mangacard"; // manga card
 import { Separator } from "@/shadcncomponents/ui/separator";
 
 export default function CarouselSlide({ data, type }: CarouselSlideProps) {
   const [chunkSize, setChunkSize] = useState(5);
 
+  const isManga = type === "manga"
   useEffect(() => {
     function updateChunkSize() {
       const width = window.innerWidth;
 
-      if (width < 640) setChunkSize(2);       // grid-cols-2
-      else if (width < 768) setChunkSize(3);  // sm:grid-cols-3
-      else if (width < 1024) setChunkSize(4); // md:grid-cols-4
-      else if (width < 1280) setChunkSize(5); // lg:grid-cols-5
-      else if (width < 1536) setChunkSize(5); // xl:grid-cols-5
-      else setChunkSize(6);                   // 2xl:grid-cols-6
+      if (width < 640) setChunkSize(2);
+      else if (width < 768) setChunkSize(3);
+      else if (width < 1024) setChunkSize(4);
+      else if (width < 1150) setChunkSize(4);
+      else if (width < 1280) setChunkSize(5);
+      else if (width < 1536) setChunkSize(10);
+      else setChunkSize(12);
     }
 
     updateChunkSize();
@@ -32,7 +28,7 @@ export default function CarouselSlide({ data, type }: CarouselSlideProps) {
     return () => window.removeEventListener("resize", updateChunkSize);
   }, []);
 
-  const slides: (Anime | Manga)[][] = [];
+  const slides: (Anime[] | Manga[])[] = [];
   for (let i = 0; i < data.length; i += chunkSize) {
     slides.push(data.slice(i, i + chunkSize));
   }
@@ -44,7 +40,7 @@ export default function CarouselSlide({ data, type }: CarouselSlideProps) {
           <CarouselItem key={slideIndex}>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-6 gap-3 mt-3 w-full">
               {group.map((item, i) =>
-                type === "manga" ? (
+                isManga ? (
                   <MangaCard key={i} data={item as Manga} />
                 ) : (
                   <AnimeCard key={i} data={item as Anime} />
@@ -55,8 +51,8 @@ export default function CarouselSlide({ data, type }: CarouselSlideProps) {
         ))}
       </CarouselContent>
 
-      <CarouselPrevious className="absolute top-1/2 h-[50%] -translate-y-1/2 -left-0 lg:hidden flex p-5" />
-      <CarouselNext className="absolute top-1/2 h-[50%] -translate-y-1/2 -right-0 lg:hidden flex p-5" />
+      <CarouselPrevious className="absolute top-1/2 h-[50%]  -translate-y-1/2 -left-0 lg:hidden flex p-5" />
+      <CarouselNext className="absolute top-1/2  h-[50%]  -translate-y-1/2 -right-0 lg:hidden flex p-5" />
       <Separator className="mt-8" />
     </Carousel>
   );
